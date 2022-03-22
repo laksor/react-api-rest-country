019e8a14f87e5f60f75ac2364c2307543b8f36c7
laskor <al.moksud@gmail.com>
2022-03-22 01:22:50 -0400

    componenets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import { useEffect, useState } from 'react';
 import './App.css';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountry></LoadCountry>
+      <Header></Header>
+      <Countries></Countries>
     </div>
   );
 }
 
-function LoadCountry (){
+/*function LoadCountry (){
   const [countries, setCountries] = useState([]);
   useEffect( () =>{
     fetch('https://restcountries.com/v3.1/all')
@@ -21,7 +24,7 @@ function LoadCountry (){
   return(
     <div>
       <h1>All country of the world</h1>
-      <h3>{countries.length}</h3>
+      <h3>Total country {countries.length}</h3>
       {
         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
       }
@@ -37,6 +40,6 @@ function Country(props){
       <h4>Population : {props.population}</h4>
     </div>
   )
-}
+}*/
 
 export default App;
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Countries.css'
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countries</h2>
+        </div>
+    );
+};
+
+export default Countries;
